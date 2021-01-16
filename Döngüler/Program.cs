ce89c9066f5c4d35c29bd3da44fd5f1d6fe80099
Kaan Demirtaş <kaandemirtas6d@gmail.com>
2021-01-17 02:04:25 +0300

    KampIntro

---
+++ Döngüler/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Döngüler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string kurs1 = "Yazılım Geliştirici Yerleştirme Kampı";
+            string kurs2 = "Programlamaya başlangıç için temel kurs";
+            string kurs3 = "Java";
+
+
+            //array - dizi
+
+            string[] kurslar = new string[] { "Yazılım Geliştirici Yerleştirme Kampı", "Programlamaya başlangıç için temel kurs", "Java" };
+
+
+
+            for (int i = 0; i < kurslar.Length ; i++)
+            {
+                Console.WriteLine(kurslar[i]);
+            }
+
+            Console.WriteLine("For bitti");
+            
+            foreach (string kurs in kurslar)
+            {
+                Console.WriteLine(kurs);
+            }
+
+            Console.WriteLine("sayfa sonu - footer");
+
+
+        }
+    }
+}
