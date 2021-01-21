9220345e47636aad98c3ec7128ca5c7744b1b2c6
Kaan Demirtaş <56064310+Brobaba290@users.noreply.github.com>
2021-01-21 14:04:55 +0300

    MyDictionary

---
+++ CustomMyList/MyList.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomMyList
+{
+    class MyList<T>
+    {
+        T[] items;
+        public MyList()
+        {
+            items = new T[0];
+        }
+        public void Add(T item)
+        {
+            T[] tempArray = items;
+            items = new T[items.Length + 1];
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                items[i] = tempArray[i];
+            }
+
+            items[items.Length - 1] = item;
+        }
+
+        public T[] Listem
+        {
+            get { return items; }
+        }
+
+        public int Count()
+        {
+            return items.Length;
+        }
+    }
+
+}
---
+++ CustomMyList/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CustomMyList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> listem = new MyList<string>();
+            listem.Add("Kaan");
+            listem.Add("Haydar");
+            listem.Add("Çağdaş");
+
+            Console.WriteLine("Adet : " + listem.Count());
+
+            foreach (var item in listem.Listem)
+            {
+                Console.WriteLine(item);
+            }
+           
+        }
+    }
+}
